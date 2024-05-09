ab454ca72e94a6b352fa559098c7bc43cd93ba8d
孙天雨 <suntianyu@bbk.com>
2024-05-09 15:43:19 +0800

    feat():[WelcomeActivity界面布局调整]

--- app/src/main/java/cn/wearbbs/note/ui/activity/WelcomeActivity.kt
+++ app/src/main/java/cn/wearbbs/note/ui/activity/WelcomeActivity.kt
@@ -2,7 +2,6 @@ package cn.wearbbs.note.ui.activity
 
 import android.os.Bundle
 import android.widget.LinearLayout
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -11,7 +10,6 @@ import cn.wearbbs.note.R
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_welcome)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
