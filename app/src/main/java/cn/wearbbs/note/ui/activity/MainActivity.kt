4345d82cda6565c6f4eedada71fd695f68b02df3
孙天雨 <suntianyu@bbk.com>
2024-05-09 15:38:59 +0800

    feat():[MainActivity添加首次静默迁移逻辑；列表删除交互调整]

--- app/src/main/java/cn/wearbbs/note/ui/activity/MainActivity.kt
+++ app/src/main/java/cn/wearbbs/note/ui/activity/MainActivity.kt
@@ -1,8 +1,11 @@
 package cn.wearbbs.note.ui.activity
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
@@ -10,7 +13,7 @@ import android.widget.EditText
 import android.widget.FrameLayout
 import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -28,17 +31,33 @@ import kotlinx.coroutines.launch
 import cn.wearbbs.note.application.MainApplication
 import cn.wearbbs.note.database.bean.Config
 import cn.wearbbs.note.database.bean.Note
+import cn.wearbbs.note.ui.activity.ui.DeleteDialog
+import cn.wearbbs.note.util.DealFile
+import java.io.File
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
+import kotlin.math.log
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var textViewTime: TextView
     private var job: Job? = null
+    private var deleteDialog: DeleteDialog? = null
+    private var adapter:NoteAdapter ?= null
+    private var dataSet : MutableList<Note> =  mutableListOf()
+    private var recyclerView: SwipeRecyclerView? = null
+    private var lyEmpty:LinearLayout?= null
+
+
+    val SEND_PERMISSIONS = arrayOf(
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
+    private val SP_FIRST_USE = "SP_first_use"
+    private val SP_IS_FIRST = "sp_is_first"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -47,7 +66,89 @@ class MainActivity : AppCompatActivity() {
         }
 
         textViewTime = findViewById(R.id.textView_time)
+        //首次进入应用 先申请权限，用户同意了静默迁移数据，拒绝正常进行下面流程
+        requestPermission()
+        val btnSettings: FrameLayout = findViewById(R.id.btn_settings)
+        btnSettings.setOnClickListener {
+            startActivity(Intent(this, SettingsActivity::class.java))
+        }
 
+        val btnCreate: FrameLayout = findViewById(R.id.btn_create)
+        val invisibleEditText: EditText = findViewById(R.id.invisible_edittext)
+        btnCreate.setOnClickListener {
+            invisibleEditText.requestFocus()
+            val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.showSoftInput(invisibleEditText, 0)
+        }
+
+        invisibleEditText.setOnKeyListener { _, keyCode, _ ->
+            if(keyCode == KeyEvent.KEYCODE_ENTER){
+                val inputText = invisibleEditText.text.toString()
+                if (TextUtils.isEmpty(inputText)){
+                    Toast.makeText(this, "文件名不能为空", Toast.LENGTH_SHORT).show()
+                    return@setOnKeyListener true
+                }
+                CoroutineScope(Dispatchers.Main).launch {
+                    MainApplication.noteDao.insertAll(Note(name = inputText, content = "", createTime = System.currentTimeMillis()))
+                    Intent(this@MainActivity, EditActivity::class.java).apply {
+                        putExtra("id", MainApplication.noteDao.findByName(inputText).id)
+                        startActivity(this)
+                    }
+                }
+                invisibleEditText.text.clear()
+                invisibleEditText.clearFocus()
+                return@setOnKeyListener true
+            }
+            false
+        }
+
+
+    }
+
+    private fun requestPermission() {
+        // 获取 SharedPreferences 实例
+        val sharedPref = getSharedPreferences(SP_FIRST_USE, Context.MODE_PRIVATE)
+        val isFirst = sharedPref.getBoolean(SP_IS_FIRST,false)
+        if (!isFirst){
+            with(sharedPref.edit()){
+                putBoolean(SP_IS_FIRST,true)
+                apply()
+            }
+            requestPermissions(SEND_PERMISSIONS, 1)
+        }else{
+            initData()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            1 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    CoroutineScope(Dispatchers.Main).launch {
+                        val list =
+                            DealFile.convertTxtToNoteList(DealFile.filterTxtFiles(DealFile.appDataDirectoryPath))
+                        list.forEach {
+                            if (it.name.isBlank()) {
+                                it.name = "未命名"
+                            }
+                        }
+                        MainApplication.noteDao.insertAll(*list.toTypedArray())
+                        DealFile.clearFolder(DealFile.appDataDirectoryPath)
+                    }
+                    initData()
+                } else {
+                    initData()
+                }
+            }
+        }
+    }
+
+    private fun initData(){
         CoroutineScope(Dispatchers.Main).launch {
             MainApplication.timeFormat = MainApplication.configDao.findByName("timeFormat")?.config ?: "24"
             if(MainApplication.configDao.findByName("firstStart")==null){
@@ -66,56 +167,23 @@ class MainActivity : AppCompatActivity() {
         }
 
         refreshData()
-
-        val btnSettings: FrameLayout = findViewById(R.id.btn_settings)
-        btnSettings.setOnClickListener {
-            startActivity(Intent(this, SettingsActivity::class.java))
-        }
-
-        val btnCreate: FrameLayout = findViewById(R.id.btn_create)
-        val invisibleEditText: EditText = findViewById(R.id.invisible_edittext)
-        btnCreate.setOnClickListener {
-            invisibleEditText.requestFocus()
-            val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
-            imm.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS);
-        }
-
-        invisibleEditText.setOnKeyListener { _, keyCode, _ ->
-            if(keyCode == KeyEvent.KEYCODE_ENTER){
-                val inputText = invisibleEditText.text.toString()
-                CoroutineScope(Dispatchers.Main).launch {
-                    MainApplication.noteDao.insertAll(Note(name = inputText, content = "", createTime = System.currentTimeMillis()))
-                    Intent(this@MainActivity, EditActivity::class.java).apply {
-                        putExtra("id", MainApplication.noteDao.findByName(inputText).id)
-                        startActivity(this)
-                    }
-                }
-                invisibleEditText.text.clear()
-                invisibleEditText.clearFocus()
-                return@setOnKeyListener true
-            }
-            false
-        }
-
-
     }
 
     private fun refreshData(){
-
         CoroutineScope(Dispatchers.Main).launch {
-            val dataSet = MainApplication.noteDao.getAll()
-            val lyEmpty:LinearLayout = findViewById(R.id.ly_empty)
-            val recyclerView:SwipeRecyclerView = findViewById(R.id.recycler_view)
+            dataSet = MainApplication.noteDao.getAll()
+            lyEmpty = findViewById(R.id.ly_empty)
+            recyclerView = findViewById(R.id.recycler_view)
             if(dataSet.isEmpty()){
-                recyclerView.visibility = View.GONE
-                lyEmpty.visibility = View.VISIBLE
+                recyclerView?.visibility = View.GONE
+                lyEmpty?.visibility = View.VISIBLE
                 return@launch
             }
             dataSet.reverse()
             runOnUiThread{
-                recyclerView.visibility = View.VISIBLE
-                lyEmpty.visibility = View.GONE
-                val adapter = NoteAdapter(dataSet, object : NoteAdapter.OnItemClickListener {
+                recyclerView?.visibility = View.VISIBLE
+                lyEmpty?.visibility = View.GONE
+                adapter = NoteAdapter(dataSet, object : NoteAdapter.OnItemClickListener {
                     override fun onItemClick(position:Int) {
                         val note = dataSet[position]
                         if(note.content.isBlank()){
@@ -128,11 +196,15 @@ class MainActivity : AppCompatActivity() {
                         }
                     }
 
+                    override fun onItemLongClick(position: Int) {
+                        showDeleteDialog(position)
+                    }
+
                 })
-                recyclerView.adapter = adapter
-                recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
-                recyclerView.setLongPressDragEnabled(true); // 拖拽排序，默认关闭。
-                recyclerView.setItemViewSwipeEnabled(true); // 侧滑删除，默认关闭。
+                recyclerView?.adapter = adapter
+                recyclerView?.layoutManager = LinearLayoutManager(this@MainActivity)
+                recyclerView?.setLongPressDragEnabled(false) // 拖拽排序，默认关闭。
+                recyclerView?.setItemViewSwipeEnabled(false) // 侧滑删除，默认关闭。
 
                 val mItemMoveListener: OnItemMoveListener =  object : OnItemMoveListener{
                     override fun onItemMove(
@@ -158,8 +230,8 @@ class MainActivity : AppCompatActivity() {
                                 }
                             })
                             MainApplication.noteDao.update(dataSet[toPosition])
-                            adapter.setDataSet(dataSet)
-                            adapter.notifyItemMoved(fromPosition, toPosition)
+                            adapter!!.setDataSet(dataSet)
+                            adapter!!.notifyItemMoved(fromPosition, toPosition)
                         }
 
                         return true
@@ -173,20 +245,49 @@ class MainActivity : AppCompatActivity() {
                         CoroutineScope(Dispatchers.Main).launch {
                             MainApplication.noteDao.delete(dataSet[position])
                             dataSet.removeAt(position)
-                            adapter.setDataSet(dataSet)
-                            adapter.notifyItemRemoved(position)
-                            adapter.notifyItemRangeChanged(position, dataSet.size - position)
+                            adapter!!.setDataSet(dataSet)
+                            adapter!!.notifyItemRemoved(position)
+                            adapter!!.notifyItemRangeChanged(position, dataSet.size - position)
                             if(dataSet.isEmpty()){
-                                recyclerView.visibility = View.GONE
-                                lyEmpty.visibility = View.VISIBLE
+                                recyclerView?.visibility = View.GONE
+                                lyEmpty?.visibility = View.VISIBLE
                             }
                         }
                     }
                 }
-                recyclerView.setOnItemMoveListener(mItemMoveListener) // 监听拖拽，更新UI。
+                recyclerView?.setOnItemMoveListener(mItemMoveListener) // 监听拖拽，更新UI。
             }
         }
     }
+
+    fun showDeleteDialog(position: Int) {
+        if (deleteDialog == null) {
+            deleteDialog = DeleteDialog(this,R.style.dialog_default_style)
+            deleteDialog?.onItemClickListener = object : DeleteDialog.OnItemClickListener {
+                override fun onCancelClick() {
+                    deleteDialog?.dismiss()
+                }
+
+                override fun onDeleteClick() {
+                    deleteDialog?.dismiss()
+                    CoroutineScope(Dispatchers.Main).launch {
+                        MainApplication.noteDao.delete(dataSet[position])
+                        dataSet.removeAt(position)
+                        adapter!!.setDataSet(dataSet)
+                        adapter!!.notifyItemRemoved(position)
+                        adapter!!.notifyItemRangeChanged(position, dataSet.size - position)
+                        if(dataSet.isEmpty()){
+                            recyclerView?.visibility = View.GONE
+                            lyEmpty?.visibility = View.VISIBLE
+                        }
+                    }
+                }
+
+            }
+        }
+        deleteDialog?.show()
+    }
+
 
     private fun startUpdatingTime() {
         var format = "HH:mm"
@@ -211,7 +312,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
-        if(hasFocus){
+        if(hasFocus) {
             refreshData()
         }
     }
